9b992bb07f45d6e661202ab4c0e3ed5c383ff4c4
cgarcia33 <cristobalgarcia2019@u.northwestern.edu>
2019-01-09 22:46:26 -0600

    closes #3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+
+class Item extends Component {
+  render() {
+    const product = this.props.product;
+    const title = product.title;
+    const price = product.price;
+    const sku = product.sku;
+
+    return (
+      <div className="shelf-item">
+        <div>
+          <img
+            src={require(`./static/data/products/${sku}_1.jpg`)}
+            alt={product.title}
+          />
+        </div>
+        <div>{title}</div>
+        <div>${price}</div>
+        <div>
+          <button type="button">Add to cart</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    const rows = [];
+    let PRODUCTS = this.props.products;
+    for (let i = 0; i < PRODUCTS.products.length; i++) {
+      rows.push(<Item product={PRODUCTS.products[i]} />);
+    }
+
+    return <div className="shelf-container">{rows}</div>;
+  }
+}
 
 class App extends Component {
   render() {
+    let PRODUCTS = require("./static/data/products.json");
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ProductTable products={PRODUCTS}> </ProductTable>
       </div>
     );
   }
